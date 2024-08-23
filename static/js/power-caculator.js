02d39987e2a026f4bee836869de4708c61ec3f95
aminkhoshsirat <aminkhoshsirat1381@gmail.com>
2024-08-23 12:27:37 +0330

    work on power

--- static/js/power-caculator.js
+++ static/js/power-caculator.js
@@ -47,6 +47,10 @@ function hddTypeFunction(){
     document.getElementById("hdd-type-list").classList.toggle("show");
 }
 
+function hddNumberFunction(){
+    document.getElementById("hdd-number-list").classList.toggle("show");
+}
+
 function opticalDriveFunction(){
     document.getElementById("optical-drive-list").classList.toggle("show");
 }
@@ -127,24 +131,29 @@ window.onclick = function (e) {
             myDropdown12.classList.remove('show');
         }
 
-        var myDropdown13 = document.getElementById("optical-drive-list");
+        var myDropdown13 = document.getElementById("hdd-number-list");
         if (myDropdown13.classList.contains('show')) {
             myDropdown13.classList.remove('show');
         }
 
-        var myDropdown14 = document.getElementById("liquid-fan-list");
+        var myDropdown14 = document.getElementById("optical-drive-list");
         if (myDropdown14.classList.contains('show')) {
             myDropdown14.classList.remove('show');
         }
 
-        var myDropdown15 = document.getElementById("fan-list");
+        var myDropdown15 = document.getElementById("liquid-fan-list");
         if (myDropdown15.classList.contains('show')) {
             myDropdown15.classList.remove('show');
         }
 
-        var myDropdown16 = document.getElementById("fan-number-list");
+        var myDropdown16 = document.getElementById("fan-list");
         if (myDropdown16.classList.contains('show')) {
             myDropdown16.classList.remove('show');
+        }
+
+        var myDropdown17 = document.getElementById("fan-number-list");
+        if (myDropdown17.classList.contains('show')) {
+            myDropdown17.classList.remove('show');
         }
 
     }
@@ -173,7 +182,7 @@ function getCpus(socket) {
         var text = "";
         for (let i = 0; i < res.length; i++) {
             text += `<li onclick="selectCpu('${ res[i].title }', '${ res[i].power }')" class="dropdown__options__cell"><input class="checkbox" type="radio"
-                                                                                           id="" value="${res[i].title}"
+                                                                                        id="" value="${res[i].title}"
                                                                                            name=""><label
                                                         class="checked__option" for="cpu-Intel"><p>${res[i].title}</p></label>
                                                     </li>`;
@@ -184,4 +193,71 @@ function getCpus(socket) {
 
 function selectCpu(title, power){
     $('#cpu-btn-text').text(title);
+}
+
+function selectMainBoard(title, power){
+    $('#main-board-btn-text').text(title);
+}
+
+function getGpuSeries(title, id){
+    $('#gpu-brand-btn-text').text(title);
+    $.get('/power/gpu-series/' + id).then(dj_res =>{
+        var text = "";
+        var res = dj_res.objects;
+        for (let i = 0; i < res.length; i++) {
+            text += `<li onclick="getGpus('${res[i].title}', ${res[i].id})" class="dropdown__options__cell"><input class="checkbox" type="radio"
+                                                                     id="" value="${res[i].title}"
+                                                                     name=""><label
+                                                                     class="checked__option"
+                                                                     for="cpu-Intel"><p>${res[i].title}</p></label>
+                                                                      </li>`;
+        }
+        $('#gpu-series-list').html(text);
+    })
+}
+
+
+function getGpus(title, id){
+    $('#gpu-series-btn-text').text(title);
+    $.get('/power/gpus/' + id).then(dj_res =>{
+        var text = "";
+        var res = dj_res.objects;
+        for (let i = 0; i < res.length; i++) {
+            text += `<li onclick="selectGpu('${res[i].title}', ${res[i].power})" class="dropdown__options__cell"><input class="checkbox" type="radio"
+                                                                     id="" value="${res[i].title}"
+                                                                     name=""><label
+                                                                     class="checked__option"
+                                                                     for="cpu-Intel"><p>${res[i].title}</p></label>
+                                                                      </li>`;
+        }
+        $('#gpu-model-list').html(text);
+    })
+}
+
+function selectGpu(title, power){
+    $('#gpu-model-btn-text').text(title);
+}
+
+function selectRam(title, power){
+    $('#ram-btn-text').text(title);
+}
+
+function selectSSD(title, power){
+    $('#ssd-btn-text').text(title);
+}
+
+function selectHDD(title, power){
+    $('#hdd-btn-text').text(title);
+}
+
+function selectOpticalDrive(title, power){
+    $('#optical-drive-btn-text').text(title);
+}
+
+function selectLiquidFan(title, power){
+    $('#liquid-fan-btn-text').text(title);
+}
+
+function selectFan(title, power){
+    $('#fan-btn-text').text(title);
 }
