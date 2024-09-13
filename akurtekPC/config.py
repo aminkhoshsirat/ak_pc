446e51244fb1ad78885146e3563e5ee2b63a92d7
Amin Khosh Sirat <141275695+aminkhoshsirat@users.noreply.github.com>
2024-09-13 21:32:42 +0330

    Update config.py

--- akurtekPC/config.py
+++ akurtekPC/config.py
@@ -1,4 +1,4 @@
-from redis import Redis
+2from redis import Redis
 
 NESHAN_API_KEY = 'web.65f0a4266dd8482b8151f738821a5554'
 
@@ -15,37 +15,3 @@ arvan_cloud_config = {
     'simin_domain': "https://akurtek-pc.s3.ir-thr-at1.arvanstorage.ir",
     'bucket_name': "akurtek-pc"
 }
-
-
-# Postgresql Config
-postgres_config = {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'akurtek_pc',
-        'USER': 'amin_dj',
-        'PASSWORD': 'aminkh1110',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-
-
-# postgres_config = {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'qpljhcndsdh_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'komm4TjHey9zPnDI6QYf',
-#         'HOST': 'qpljhcndmnkzkweu-snj-service',
-#         'PORT': '5432'
-#     }
-
-# redis_host = 'pzmekfhdajhbvghejcnv-jyl-service'
-#
-# redis_password = 'zjtUD6AEZ9oft8nLljHt'
-#
-# redis_cli = Redis(host=redis_host, password=redis_password, db=0)
-
-
-redis_host = 'localhost'
-
-redis_password = ''
-
-redis_cli = Redis(host=redis_host, password=redis_password, db=0)
